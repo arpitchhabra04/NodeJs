2a14364073627023e893891fc6d13a2419346d87
Arpit Chhabra <chhabra.arpi@gmail.com>
2019-08-18 16:05:40 +0530

    Passport

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -11,6 +11,8 @@ var usersRouter = require("./routes/users");
 var dishRouter = require("./routes/dishRouter");
 var promoRouter = require("./routes/promoRouter");
 var leaderRouter = require("./routes/leaderRouter");
+var passport = require("passport");
+var authenticate = require("./authenticate");
 var app = express();
 
 const mongoose = require("mongoose");
@@ -45,13 +47,15 @@ app.use(
     store: new FileStore()
   })
 );
+app.use(passport.initialize());
+app.use(passport.session());
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 function auth(req, res, next) {
-  console.log(req.session);
+  console.log(req.user);
 
-  if (!req.session.user) {
+  if (!req.user) {
     var err = new Error("You are not authenticated!");
     err.status = 403;
     return next(err);
@@ -59,8 +63,6 @@ function auth(req, res, next) {
     if (req.session.user === "authenticated") {
       next();
     } else {
-      var err = new Error("You are not authenticated!");
-      err.status = 403;
       return next(err);
     }
   }
